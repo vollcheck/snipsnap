ccd4d716f1515eedd743af8c253e709158948013
vollcheck <jaw@juxt.pro>
2022-10-31 19:32:54 +0100

    tests for snap

---
+++ test/snipsnap/models/snap_test.clj
@@ -0,0 +1,47 @@
+(ns snipsnap.models.snap-test
+  (:require [clojure.test :refer [deftest is use-fixtures testing]]
+            [snipsnap.models.snap :as s]
+            [snipsnap.models.test-utils :refer [with-test-db test-db]]))
+
+(use-fixtures :once with-test-db)
+
+(deftest snap-model-test
+  (testing "standard snap get"
+    (let [existing (s/get-snap-by-id @test-db 1)]
+      (is (= (keys existing) [:snap/id
+                              :snap/user_id
+                              :snap/name
+                              :snap/content
+                              :snap/language_id
+                              :snap/create_date
+                              :snap/update_date]))))
+
+  (testing "get language by id when lang doesn't exist"
+    (is (= nil (s/get-snap-by-id @test-db 0))))
+
+  (testing "create new snap"
+    (let [new-lang (s/save-snap @test-db {:name "multiplication of two integers"
+                                          :content "const multiply = (x, y) => x * y"
+                                          :language_id 2
+                                          :user_id 1})
+          new-lang-id (second (first new-lang))]
+      (is (= 2 new-lang-id))))
+
+  (testing "list all snaps"
+    (is (= 2 (count (s/get-snaps @test-db)))))
+
+  (testing "update existing snap"
+    (let [result (s/save-snap @test-db {:id 1
+                                        :name "sum of two integers with doc"
+                                        :content "(defn sum \"doc here\" [x y] (+ x y))"
+                                        :language_id 1
+                                        :user_id 1})]
+      (is (= 1 (:next.jdbc/update-count result)))))
+
+  (testing "remove existing language"
+    (let [result (s/delete-snap-by-id @test-db 2)]
+      (is (= 1 (:next.jdbc/update-count result)))))
+
+  (testing "trying to remove language that doesn't exist"
+    (let [result (s/delete-snap-by-id @test-db 100)]
+      (is (= 0 (:next.jdbc/update-count result))))))
