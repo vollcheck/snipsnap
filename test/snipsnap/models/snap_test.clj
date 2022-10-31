9eb0b107e81c5390ed2ea2a8e3269c3036585ed4
vollcheck <jaw@juxt.pro>
2022-10-31 19:55:15 +0100

    fix obsolete typos from lang to snap tests

--- test/snipsnap/models/snap_test.clj
+++ test/snipsnap/models/snap_test.clj
@@ -16,16 +16,16 @@
                               :snap/create_date
                               :snap/update_date]))))
 
-  (testing "get language by id when lang doesn't exist"
+  (testing "get snap by id when snap doesn't exist"
     (is (= nil (s/get-snap-by-id @test-db 0))))
 
   (testing "create new snap"
-    (let [new-lang (s/save-snap @test-db {:name "multiplication of two integers"
+    (let [new-snap (s/save-snap @test-db {:name "multiplication of two integers"
                                           :content "const multiply = (x, y) => x * y"
                                           :language_id 2
                                           :user_id 1})
-          new-lang-id (second (first new-lang))]
-      (is (= 2 new-lang-id))))
+          new-snap-id (second (first new-snap))]
+      (is (= 2 new-snap-id))))
 
   (testing "list all snaps"
     (is (= 2 (count (s/get-snaps @test-db)))))
@@ -38,10 +38,10 @@
                                         :user_id 1})]
       (is (= 1 (:next.jdbc/update-count result)))))
 
-  (testing "remove existing language"
+  (testing "remove existing snap"
     (let [result (s/delete-snap-by-id @test-db 2)]
       (is (= 1 (:next.jdbc/update-count result)))))
 
-  (testing "trying to remove language that doesn't exist"
+  (testing "trying to remove snap that doesn't exist"
     (let [result (s/delete-snap-by-id @test-db 100)]
       (is (= 0 (:next.jdbc/update-count result))))))
