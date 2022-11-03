c1be662bf0de282933af6e69c9e756bced959777
vollcheck <jaw@juxt.pro>
2022-11-03 23:01:04 +0100

    add http client for client :^)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
 
+import FetchClient from "./client/fetch.client";
 import React from "react";
+import config from "./config";
 import logo from "./logo.svg";
 
 function App() {
+  const client = new FetchClient();
+  const resp = client.get(config.BASE_URL + "/");
+  resp.then((responseData) => console.log(responseData["message"]));
+
   return (
     <div className="App">
       <header className="App-header">
